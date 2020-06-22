ec26e1e7cc90f25e28e26c3f1728368fe1aa4f38
b1mind <brent@1mindsolutions.com>
2020-06-21 22:40:44 -0600

    📱working mobile nav - needs polished

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 import './app.js'
+
+const navMain = document.querySelector('.navMain')
+const navSecondary = document.querySelector('.navSecondary')
+const canHazBurger = document.querySelector('.canHazBurger')
+
+canHazBurger.addEventListener('click', e => {
+	navMain.classList.toggle('hidden')
+	navSecondary.classList.toggle('hidden')
+})
